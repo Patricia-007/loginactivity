1ec258c3daf5fc225183c170de1b52967bbb5eb4
Patricia Aguilar <Patricia Aguilar>
2017-07-17 10:23:26 -0400

    init commit

---
+++ app/src/main/java/com/example/sfstem13/finalpro/PageOne.java
@@ -0,0 +1,13 @@
+package com.example.sfstem13.finalpro;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PageOne extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_page_one);
+    }
+}
